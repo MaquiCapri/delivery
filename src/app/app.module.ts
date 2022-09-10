bfb0cf8cc7f88dfcb43d151b1b21d632955c1759
maquiCapri <maquicapri@gmail.com>
2022-09-10 10:36:24 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VistaComponent } from './vista/vista.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { SectionComponent } from './componentes/section/section.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { MenuComponent } from './componentes/header/menu/menu.component';
+import { UbicacionComponent } from './componentes/nav/ubicacion/ubicacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaComponent,
+    HeaderComponent,
+    NavComponent,
+    SectionComponent,
+    FooterComponent,
+    MenuComponent,
+    UbicacionComponent
   ],
   imports: [
     BrowserModule,
